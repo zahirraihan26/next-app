b5dfbe5e9cf115f8270d25bdf9e75c23a89e31bb
zahirraihan26 <mdzahirraihanbakul@gmail.com>
2026-03-03 23:34:58 +0600

    added api

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -62,9 +62,11 @@ export default function Blog() {
                                     {blog.title}
                                 </h3>
                                 <p className="text-zinc-600 dark:text-zinc-300 mb-4 line-clamp-3">
+                                    
                                     {blog.excerpt}
                                 </p>
                                 <div className="mt-auto">
+
                                     <a
                                         href="#"
                                         className="inline-flex items-center text-blue-600 hover:text-blue-700 font-semibold"
